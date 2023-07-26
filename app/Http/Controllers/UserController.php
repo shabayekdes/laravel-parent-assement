93a986f15e12496c99744807663acf0df4b0d696
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-07-27 01:35:12 +0300

    Update user controller with data provider manager logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,25 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\DataProviders\DataProviderContract;
+use App\Dto\DtoUserX;
+use App\Dto\DtoUserY;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class UserController extends Controller
 {
+    private DataProviderContract $providers;
+
+    public function __construct(DataProviderContract $providers)
+    {
+        $this->providers = $providers;
+    }
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        $data = collect([]);
+        $users = $this->providers->make()
+            ->getUsers()
+            ->when($request->has('provider'), function ($query) use ($request) {
+                return $query->where('provider', $request->get('provider'));
+            })->when($request->has('statusCode'), function ($query) use ($request) {
+                return $query->where('status_label', $request->get('statusCode'));
+            })->when($request->has(['balanceMin', 'balanceMax']), function ($query) use ($request) {
+                return $query->whereBetween('amount', $request->only(['balanceMin', 'balanceMax']));
+            })
+            ->values();
 
-        foreach (config('data_providers.providers') as $filePath) {
-            if (\File::exists($filePath)) {
-                $jsonContent = \File::json($filePath);
-
-                $data = $data->merge($jsonContent);
-            }
-        }
-
-        return response()->json($data);
+        return response()->json($users);
     }
 }
