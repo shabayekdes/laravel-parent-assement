8c7c663d9e021a8a07547d062961f0c0c3e42fc6
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-07-26 19:15:22 +0300

    Add list users api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Packages\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
@@ -11,6 +11,16 @@ class UserController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $data = collect([]);
+
+        foreach (config('data_providers.providers') as $filePath) {
+            if (\File::exists($filePath)) {
+                $jsonContent = \File::json($filePath);
+
+                $data = $data->merge($jsonContent);
+            }
+        }
+
+        return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Packages\Http\Controllers\UserController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
