750cac174b7981fbe92ca0076a2f779df50a95b9
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-07-27 01:33:52 +0300

    Update data providers config

--- config/data_providers.php
+++ config/data_providers.php
@@ -1,10 +1,13 @@
 <?php
 
 
+use App\Dto\DtoUserX;
+use App\Dto\DtoUserY;
+
 return [
     'providers' => [
-        database_path('DataProviderX.json'),
-        database_path('DataProviderY.json')
+        DtoUserX::class => database_path('DataProviderX.json'),
+        DtoUserY::class => database_path('DataProviderY.json')
         // Add more data providers here as needed
     ],
 ];
